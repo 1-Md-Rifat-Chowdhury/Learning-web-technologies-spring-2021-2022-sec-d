406145cb141b0a8d1c9b19b930c1e4ccb5865c01
Reyam1 <1.md.rifat,chowdhury@gmail.com>
2022-02-16 23:28:48 +0600

    lab 5

---
+++ MID_LAB_TASK_05_PHP_FORM/LAB-5.2 [PHP][Session Tracking]/Login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		Log in
+	</title>
+</head>
+<body>
+
+	<form method="POST"  action="">
+		
+			
+
+			<legend>
+
+				<table border="1" width="40%">
+					<tr>
+					
+					<td height="80px">Company<center><dr>Home<></dr><dr>Login<></dr>|<dr><>Registration</dr></center></td>
+
+					
+
+
+					</tr>
+					<tr >
+						<td height="200px" width="40px">Welcome to XCompany</td>
+					</tr>
+
+					<tr>
+						<td height="40px"><center>Copyright @2017</center> </td>
+					</tr>
+				</table>
+				
+			</legend>
+		
+	</form>
+		
+	
+
+</body>
+</html>
